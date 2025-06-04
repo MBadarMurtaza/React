748dad72d33235f86894e51f7edcb49a215adf6c
mBadar-dev <m.badar.dev@gmail.com>
2025-06-04 12:34:53 +0500

    Assignment

---
+++ context api/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/userContext";
+
+function Login() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const { setUser } = useContext(UserContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setusername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setpassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
